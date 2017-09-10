a2bb7631ab3e7f1589e9fa7445e5c689465c918d
keenguy <keenguy@qq.com>
2017-09-10 22:11:38 +0800

    some fixing

--- README.md
+++ README.md
@@ -1,27 +1,79 @@
-This is a forked repo adapted to bloging 
+This repo is forked from [mume](https://github.com/shd101wyy/mume), adapted to bloging 
 1. added index.ts, changed the main entry in package.json to it.
 1. added mume-blog.ts
 2. added blog-engine.ts, which is a modification copy of markdown-engine.ts
 3. added to changed: needToCopy/, bin/, package.json
 
-The forked repo is [mume](https://github.com/shd101wyy/mume)
-
 ## Install
 `npm install -g git+https://github.com/keenguy/wret.git#blog`
 
-## Usage
-There are mainly 3 steps:
-1. `wret init`   initialize a _config.yml file
-2. `wret build`  build the whole site into ./docs
-3. `wret deploy` git deploy to the repository configured in _config.yml
+## How to use
+1. Prepare files
+* src/: md source directory
+* assets/: css files, images
+* CNAME
+* _config.yml
+    * Use `wret i` to initialize _config.yml if needed
+    * Three fields: title, copyFiles, deploy are needed. (May be empty)
+    * srcDir, buildDir are defaulted to be 'src' and 'docs' respectively.
 
-Other commands:
+2. Build the blog: `wret b`
+
+3. Push to github `wret d`
+
+4. Other commands:
 * `wret help`
 * `wret copy`   Copy two things to buildDir: 1. built-in assets  2. files specified in the 'copyFiles' field of _config.yml
 * `wrep less`   Compile less files in 'assets/css/'
 * `wret note`   Generate htmls in buildDir using md files in srcDir
 
+## Dev log
+>2017-10-10
+Changed module name mume-blog to wret, implemented _config.yml
+
+>2017-09-09
+Integrated all notes-independent functions to mume-blog
+
+>2017-09-05
+Integrated main code into [@keenguy/mume-blog](https://github.com/keenguy/mume-blog)
+
+>2017-09-04
+added pagenator; optimized index assembly, listing items in order; rewrite index.js and gulpfile.js;
+
+>2017-08-24
+added font-awesome, ribbon, made some js opt, took some notes
+
+>2017-08-23
+
+Added some icons (e.g. folder, tag). Implemented gulp automation. Solved puml doesn't exit problem. Added some metadata. Changed some css (e.g. toc).
+
+>2017-08-22
+
+Added file-brower-like navigation. Auto generate index files for sub folders. 
+Changed mume module: 
+1. move toc-btn to top (change css)   
+2. add callback to htmlExport function in markdown-engine.js.
+>2017-08-21
+
+Implemented html exporting in batch using mume. Set up blog on github.
+
+## Some notes
+
+1. font-awesome icons doesn't work in vscode preview .
 
 
-
+## To-do
+- [ ] project layout
+- [ ] search functionality
+- [ ] comment module
+- [ ] footer with flexible templating
+- [ ] improve the layout of index page.
+- [ ] optimize for mobile
+- [ ] travis CI deployment
+- [x] improve UI (e.g. index of sub folders).
+- [x] generate subfolder index automatically
+- [x] add header or back links on each page automatically.
+- [x] metadata
+- [x] implement auto flow using gulp
+- [x] font-awesome icons (only for blog export)
 
--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,32 @@ import * as del from 'del';
 import * as utility from './utility'
 
 const cwd = process.cwd();
-const srcDir = path.resolve(cwd, 'src');
-const buildDir = path.resolve(cwd, 'docs');
-let siteConfig:any = {
-    title: "Yong's Blog",
-    url: "blog.yonggu.me",
-    gitRepo: "https://github.com/keenguy/blogit"
-}
+let srcDir = path.resolve(cwd, 'src');
+let buildDir = path.resolve(cwd, 'docs');
+let siteConfig: any = {}
 
 async function entry(arg) {
 
     arg = arg || process.argv[2] || '';
 
-    
-    await fs.readFile(path.resolve(cwd,'_config.yml'),'utf-8').then((yamlStr) => {
-        let config = utility.parseYAML(yamlStr);
-        siteConfig = Object.assign(siteConfig,config);
-    })
-    .catch((err)=>console.log("_config.yml does not exist! Default configurations are used."));
 
+    await fs.readFile(path.resolve(cwd, '_config.yml'), 'utf-8').then((yamlStr) => {
+        siteConfig = utility.parseYAML(yamlStr);
+    })
+        .catch((err) => {
+            console.log("_config.yml does not exist! Try 'wret init'.");
+            return;
+        });
+    if (!siteConfig.title || !siteConfig.copyFiles || !siteConfig.deploy) {
+        console.log("Error: Something is wrong with _config.yml ");
+        return;
+    }
+    if (siteConfig.srcDir) {
+        srcDir = path.resolve(cwd,siteConfig.srcDir);
+    }
+    if (siteConfig.buildDir) {
+        buildDir = path.resolve(cwd,siteConfig.buildDir);
+    }
     const site = new MumeBlog(srcDir, buildDir, siteConfig);
 
     if (arg == 'help' || arg == '') {
@@ -39,7 +46,7 @@ async function entry(arg) {
         less(l) --- Compile less files in 'assets/css/'
         deploy(d) --- Push buildDir to git repo configured in the deploy field of _config.yml
         `)
-    } else if(arg == 'init' || arg=='i'){
+    } else if (arg == 'init' || arg == 'i') {
         init();
     }
     else if (arg == 'copy' || arg == 'c') {
@@ -48,18 +55,18 @@ async function entry(arg) {
         site.generateHtmls();
     } else if (arg == 'deploy' || arg == 'd') {
         deploy();
-    } else if (arg == 'less' || arg == 'l'){
+    } else if (arg == 'less' || arg == 'l') {
         await compileCss().then(() => console.log("(^_^) All less files in assets/ compiled."));
     }
-    else if(arg == 'build' || arg == 'b'){
+    else if (arg == 'build' || arg == 'b') {
         site.generateHtmls();  // generate htmls based on 'srcDir', writing to 'buildDir'
         await compileCss().then(() => console.log("(^_^) All less files in assets/ compiled."));
         copyData();
     }
 }
 
-function init(){
-    fs.copy(path.resolve(__dirname, "../../needToCopy/_config.yml"), path.resolve(cwd,'_config.yml')).then(()=>{
+function init() {
+    fs.copy(path.resolve(__dirname, "../../needToCopy/_config.yml"), path.resolve(cwd, '_config.yml')).then(() => {
         console.log("_config.yml are initialized.");
     })
 }
@@ -71,10 +78,11 @@ function init(){
 async function copyData() {
 
     let asyncEvents = [];
-    asyncEvents.push(fs.copy(path.resolve(__dirname, "../../needToCopy/assets"), path.resolve(buildDir,'assets')));
+    await fs.copy(path.resolve(__dirname, "../../needToCopy/assets"), path.resolve(buildDir, 'assets'))
+    .catch((err)=>console.log('needToCopy',err));
     if (siteConfig.copyFiles) {
         siteConfig.copyFiles.forEach(filePath =>
-            asyncEvents.push(fs.copy(path.resolve(cwd, filePath), path.resolve(buildDir, filePath))));
+            asyncEvents.push(fs.copy(path.resolve(cwd, filePath), path.resolve(buildDir, filePath)).catch((err)=>console.log(err))));
     }
     await Promise.all(asyncEvents).then(() => console.log('(^_^) Copy assets and other files succeed!'));
 }
@@ -103,8 +111,8 @@ function lessify(str, options) {
 }
 
 function deploy() {
-    const repo = siteConfig.deploy && siteConfig.deploy.repo || '';
-    if (!repo){
+    const repo = siteConfig.deploy.repo || '';
+    if (!repo) {
         console.log("Deploy failed: no git repository.");
     }
     const branch = siteConfig.deploy.branch || 'master';
@@ -114,7 +122,7 @@ function deploy() {
         'docs/.gitignore'
     ]).then(() => {
         // starting a new repo
-        require('simple-git')(__dirname + '/docs')
+        require('simple-git')(buildDir)
             .outputHandler(function (command, stdout, stderr) {
                 stderr.pipe(process.stderr);
             })
